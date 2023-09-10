f9e86cb1e55608526e3363eb53ef549c388d5b69
SeongHyeon.Seo <tintachina84@gmail.com>
2023-09-10 23:18:23 +0900

    Fixed: application.yml scope indent was long.

--- src/main/java/com/tintachina/blog/domain/User.java
+++ src/main/java/com/tintachina/blog/domain/User.java
@@ -26,7 +26,7 @@ public class User implements UserDetails {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "password")
     private String password;
 
     @Column(name = "nickname", unique = true)
